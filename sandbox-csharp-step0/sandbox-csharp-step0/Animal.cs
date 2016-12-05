16ce17f48904ee46c02e9255db255397475c066d
JVanhouteghem <vanhouteghem.j@gmail.com>
2016-12-05 10:15:00 +0100

    start commit

---
+++ sandbox-csharp-step0/sandbox-csharp-step0/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandbox_csharp_step0
+{
+    class Animal
+    {
+        private string specie;
+
+        public string GetSpecie()
+        {
+            return specie;
+        }
+
+        public void SetSpecie(string value)
+        {
+            specie = value;
+        }
+
+    }
+}
---
+++ sandbox-csharp-step0/sandbox-csharp-step0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandbox_csharp_step0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Tapoulet");
+            Console.ReadLine();
+
+
+            string stringName = "Hello World !";
+            
+        }
+    }
+}
