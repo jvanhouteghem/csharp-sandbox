6144b2b36333437f3d2af753c7c24fb32bf004c4
JVanhouteghem <vanhouteghem.j@gmail.com>
2016-12-07 09:23:50 +0100

    commit step4

---
+++ sandbox-csharp-step4/sandbox-csharp-step4/Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace sandbox_csharp_step4 {
+    partial class Form1 {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(-7, -37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 382);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 338);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ sandbox-csharp-step4/sandbox-csharp-step4/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sandbox_csharp_step4 {
+    public partial class Form1 : Form {
+
+        // Graphics est l'élément sur lequel on dessine
+        private Graphics g;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) {
+
+            // Méthode similaire
+            g = panel1.CreateGraphics();
+
+            int panelWidth = this.Size.Width;
+            int panelHeight = this.Size.Height;
+            Console.WriteLine("h : " + panelHeight + " - " + "w" + panelWidth);
+
+            Pen blackPen = new Pen(Color.Black, 3);
+            Brush[] myBrush = new Brush[9];
+            myBrush[0] = new SolidBrush(Color.Gray);
+            myBrush[1] = new SolidBrush(Color.DarkRed);
+            myBrush[2] = new SolidBrush(Color.Yellow);
+            myBrush[3] = new SolidBrush(Color.Violet);
+            myBrush[4] = new SolidBrush(Color.Blue);
+            myBrush[5] = new SolidBrush(Color.Brown);
+            myBrush[6] = new SolidBrush(Color.Green);
+            myBrush[7] = new SolidBrush(Color.Black);
+            myBrush[8] = new SolidBrush(Color.Orange);
+
+            // Create location and size of ellipse.
+            // xPanel, le centre du grand cercle
+            float xPanel = panelWidth/6;
+            float yPanel = panelHeight/6;
+            float widthEllipse = 200.0F; // 355; 314
+            float heightEllipse = 200.0F;
+
+            float rayon = widthEllipse / 2;
+
+            float centerCerclePrincipalX = panelWidth / 2 - rayon;
+            float centerCerclePrincipalY = panelHeight / 2 - rayon;
+
+            // Draw ellipse to screen.
+            g.DrawEllipse(blackPen, centerCerclePrincipalX, centerCerclePrincipalY, widthEllipse, heightEllipse);
+
+            for (int i = 1; i < 10; i++) {
+
+                double angle = Math.PI/2;
+                angle += Math.PI * 2 / 9 * i;
+
+                float rayonPetitCercle = 15;
+
+                double positionXCercle = panelWidth/2 - Math.Cos(angle) * rayon - rayonPetitCercle/2;
+                double positionYCercle = panelHeight/2 - Math.Sin(angle) * rayon - rayonPetitCercle/2;              
+
+                g.FillEllipse(myBrush[i-1], (float) positionXCercle, (float) positionYCercle, rayonPetitCercle, rayonPetitCercle);
+
+            }
+        }
+    }
+}
